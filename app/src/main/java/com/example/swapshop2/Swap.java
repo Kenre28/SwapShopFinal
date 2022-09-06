bcc99b842853efcac296f91a39f0f71c0d7ef8d3
Rashay Daya <rashay.jcdaya@gmail.com>
2022-09-07 00:07:57 +0200

    Swap

---
+++ app/src/main/java/com/example/swapshop2/Swap.java
@@ -0,0 +1,27 @@
+package com.example.swapshop2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Swap extends AppCompatActivity {
+    Button btnSWContact;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swap);
+
+        btnSWContact = findViewById(R.id.btnSWContact);
+        btnSWContact.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Chat.class));
+            }
+        });
+
+    }
+}
